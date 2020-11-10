a07716278a82d98156d4b71dca2bc9cc508b7cd3
blindreviewsrc <blindreviewresources@gmail.com>
2020-11-10 16:13:54 -0300

    start project blind

--- plugin.py
+++ plugin.py
@@ -1,11 +1,10 @@
-from v1.v1 import NVCCPlugin as NVCC_V1
-from v2.v2 import NVCCPluginV2 as NVCC_V2
-from c.c import CPlugin as C
-from cpp.cpp import CPPPlugin as CPP
-from java.java import JavaPlugin as JAVA
-from verilog.verilog import VERILOGPlugin as VERILOG
-from valgrind.valgrind import ValgrindPlugin as Valgrind
-from gem5.gem5 import Gem5Plugin as GEM5
+from src.v1.v1 import NVCCPlugin as NVCC_V1
+from src.c.c import CPlugin as C
+from src.cpp.cpp import CPPPlugin as CPP
+from src.java.java import JavaPlugin as JAVA
+from src.verilog.verilog import VERILOGPlugin as VERILOG
+from src.valgrind.valgrind import ValgrindPlugin as Valgrind
+from src.gem5.gem5 import Gem5Plugin as GEM5
 
 
 def load_ipython_extension(ip):
